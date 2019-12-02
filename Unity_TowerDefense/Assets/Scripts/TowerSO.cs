003dca697ae863c25b9902c4af4eba0f44bc1670
SynPyre <58309964+SynPyre@users.noreply.github.com>
2019-12-02 15:13:29 -0500

    TowerSO

---
+++ Unity_TowerDefense/Assets/Scripts/TowerSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TowerType
+{
+    RANGED,
+    MELEE,
+    MAGIC,
+    ARTILLERY
+}
+[CreateAssetMenu(fileName = "New Tower", menuName = "Tower/New Tower", order = 1)]
+public class TowerSO : ScriptableObject
+{
+    public TowerType type;
+
+    public int level;
+    public int buildCost;
+    public int damage;
+    public float attackRate;
+    public int range;
+    public GameObject towerPrefab;
+
+    public TowerSO nextUpgrade;
+
+
+}
