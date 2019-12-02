397a4df750efccea2321d520fd38b1032e387c0c
Jane <glazychevaeo@gmail.com>
2019-12-02 12:54:45 -0500

    Scenes

---
+++ Unity_TowerDefense/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 30f;
+
+    private void Update()
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate( Vector3.up * rotationSpeed);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate( Vector3.up * -rotationSpeed);
+            }
+        }
+}
