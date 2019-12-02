7306a59e50bd209bb43da73af85cc0e9e0bbfaa7
F2PM-Vein <F2PM-Vein@users.noreply.github.com>
2019-12-02 13:50:59 -0500

    Main Menu Done

---
+++ Unity_TowerDefense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+    public GameObject newGameBtns;
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void newGame()
+    {   
+            newGameBtns.SetActive(!newGameBtns.activeSelf);
+    }
+    public void mainMenuToggle()
+    {
+        menuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+
+    }
+
+    public void settingsMenuToggle()
+    {
+        settingsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void CreditsMenuToggle()
+    {
+        creditsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("I quit");
+        Application.Quit();
+    }
+}
