e590c6311de2d8c5e3f835ff61805a8e72cf4f27
SynPyre <58309964+SynPyre@users.noreply.github.com>
2019-12-04 18:14:12 -0500

    Level SO + rest of Enemy SO

---
+++ Unity_TowerDefense/Assets/Scripts/SO/LevelSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level", menuName = "Level/New Level", order = 1)]
+public class LevelSO : ScriptableObject
+{
+    public string levelName;
+    
+}
