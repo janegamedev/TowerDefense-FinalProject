af3c3981adfbf67034d2ff512f4fc31b83e4e2ef
Jane <glazychevaeo@gmail.com>
2019-12-04 23:27:20 -0500

    Camera controller

--- Unity_TowerDefense/Assets/Scripts/CameraController.cs
+++ Unity_TowerDefense/Assets/Scripts/CameraController.cs
@@ -1,21 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 30f;
+    [SerializeField] private int horizontalMinBorder;
+    [SerializeField] private int horizontalMaxBorder;
+    [SerializeField] private int verticalMinBorder;
+    [SerializeField] private int verticalMaxBorder;
+    
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float zoomSpeed;
+    
+    [SerializeField] private float hMax = 20f;
+    private float _hNormal;   
+    
+    [SerializeField] private float rMin = 30f;
+    private float _rNormal;
+    
+    private Camera _camera;
+
+    private void Start()
+    {
+        _hNormal = transform.position.y;
+        _rNormal = transform.rotation.x;
+        
+        _camera = Camera.main;
+    }
 
     private void Update()
         {
-            transform.position = transform.position  + Vector3.right *  Input.GetAxis("Horizontal") * movementSpeed;
-            transform.position = transform.position  + Vector3.forward *  Input.GetAxis("Vertical") * movementSpeed;
-            
-            /*if (Input.GetAxis("Horizontal") != 0)
+            if (Input.GetKey(KeyCode.W) && transform.position.z <= verticalMaxBorder)
             {
-                transform.Rotate( Vector3.up * rotationSpeed);
+                transform.position += Vector3.forward * movementSpeed;
             }
-            else if (Input.GetKey(KeyCode.E))
+            
+            if (Input.GetKey(KeyCode.S) && transform.position.z >= verticalMinBorder)
             {
-                transform.Rotate( Vector3.up * -rotationSpeed);
-            }*/
+                transform.position += Vector3.back * movementSpeed;
+            }
+            
+            if (Input.GetKey(KeyCode.A) && transform.position.x >= horizontalMinBorder)
+            {
+                transform.position += Vector3.left * movementSpeed;
+            }
+            
+            if (Input.GetKey(KeyCode.D) && transform.position.x <= horizontalMaxBorder)
+            {
+                transform.position += Vector3.right * movementSpeed;
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f ) // forward
+            {
+                ZoomIn();
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f ) // backwards
+            {
+                ZoomOut();
+            }
         }
+
+    void ZoomIn()
+    {
+        if (_camera.fieldOfView > hMax)
+        {
+            transform.position = new Vector3(transform.position.x,Mathf.Lerp(transform.position.y, hMax, Time.deltaTime * zoomSpeed), transform.position.z);
+            //lerp angle
+        }
+    }
+
+    void ZoomOut()
+    {
+        if (_camera.fieldOfView < _hNormal)
+        {
+            transform.position = new Vector3(transform.position.x,Mathf.Lerp(transform.position.y, _hNormal, Time.deltaTime * zoomSpeed), transform.position.z);
+            //lerp angle
+        }
+    }
 }
