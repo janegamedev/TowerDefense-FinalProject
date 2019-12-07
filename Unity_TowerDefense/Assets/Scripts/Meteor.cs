494015621f9c30c1abc5bed6e15dcb31f7ee4c79
Jane <glazychevaeo@gmail.com>
2019-12-06 23:03:37 -0500

    Meteor skill

---
+++ Unity_TowerDefense/Assets/Scripts/Meteor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    public float range;
+    public float damage;
+    public bool isExploded;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<RoadTile>())
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Debug.Log("Here");
+        if (!isExploded)
+        {
+            isExploded = true;
+            Collider[] enemies = Physics.OverlapSphere(transform.position, range , LayerMask.GetMask("Enemy"));
+
+            foreach (Collider enemy in enemies)
+            {
+                Enemy e = enemy.GetComponent<Enemy>();
+                e.TakeHit(damage, DamageType.PHYSICAL);
+            }
+        
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity_TowerDefense/Assets/Scripts/MeteorSkill.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeteorSkill : MonoBehaviour
+{
+    public int damage;
+    public int range;
+
+    public Button skillButton;
+    public GameObject meteor;
+    public float resetTime;
+    private Vector3 _destination;
+    private bool _isSelected;
+    private bool _canFire;
+    private Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+        skillButton.onClick.AddListener(Select);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isSelected)
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Road")))
+            {
+                _destination = hit.transform.position;
+                _canFire = true;
+            }
+            else
+            {
+                _canFire = false;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Fire();
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                _isSelected = false;
+                skillButton.interactable = true;
+            }
+        }
+    }
+
+    private void Select()
+    {
+        _isSelected = true;
+        skillButton.interactable = false;
+    }
+
+    private void Fire()
+    {
+        if (_canFire)
+        {
+            skillButton.interactable = false;
+            
+            Debug.Log("Fire!");
+            _isSelected = false;
+            _canFire = false;
+            StartCoroutine(Restart());
+
+            Meteor m = Instantiate(meteor, _destination + Vector3.up * 150, Quaternion.identity).GetComponent<Meteor>();
+            m.damage = damage;
+            m.range = range;
+            
+            _destination = Vector3.zero;
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(resetTime);
+        skillButton.interactable = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_destination,range);
+    }
+}
