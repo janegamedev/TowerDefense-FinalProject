94bf4d111ae736cd687af3255c1d1f558d43d3a4
Jane <glazychevaeo@gmail.com>
2019-12-08 18:51:21 -0500

    Loading screen

--- Unity_TowerDefense/Assets/Scripts/Ui/MainMenuUi.cs
+++ Unity_TowerDefense/Assets/Scripts/Ui/MainMenuUi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+public class MainMenuUi : MonoBehaviour
 {
     public GameObject menuPanel;
     public GameObject settingsPanel;
@@ -15,39 +15,30 @@ public class MainMenu : MonoBehaviour
     {   
             newGameBtns.SetActive(!newGameBtns.activeSelf);
     }
-    public void MinMenuToggle()
-    {
-        menuPanel.SetActive(!menuPanel.activeSelf);
-        settingsPanel.SetActive(!settingsPanel.activeSelf);
-        creditsPanel.SetActive(!creditsPanel.activeSelf);
-        newGameBtns.SetActive(!newGameBtns.activeSelf);
-    }
-
+    
     public void SettingsMenuToggle()
     {
-
         settingsPanel.SetActive(!settingsPanel.activeSelf);
         menuPanel.SetActive(!menuPanel.activeSelf);
-        newGameBtns.SetActive(!newGameBtns.activeSelf);
+        newGameBtns.SetActive(false);
     }
 
     public void CreditsMenuToggle()
     {
         creditsPanel.SetActive(!creditsPanel.activeSelf);
         menuPanel.SetActive(!menuPanel.activeSelf);
-        newGameBtns.SetActive(!newGameBtns.activeSelf);
+        newGameBtns.SetActive(false);
     }
 
     public void QuitMenuToggle()
     {
         quitPanel.SetActive(!quitPanel.activeSelf);
-        newGameBtns.SetActive(!newGameBtns.activeSelf);
+        newGameBtns.SetActive(false);
     }
 
     public void Quit()
     {
         Debug.Log("I quit");
-        
         Application.Quit();
     }
 }
